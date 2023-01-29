865bc4aba0aaedd67dfa306570407598e1c98691
Samuel Huang <56812343+upieez@users.noreply.github.com>
2023-01-29 16:07:05 +0800

    add doc string

--- final_project/translator.py
+++ final_project/translator.py
@@ -1,15 +1,15 @@
-from ibm_watson import language_translator_v3
+"""language service provided by ibm"""
+from ibm_watson import LanguageTranslatorV3
 
-translator = language_translator_v3.LanguageTranslatorV3(version="2018-05-01")
+translator = LanguageTranslatorV3(version="2018-05-01")
 
 class MachineTranslation():
-    def __init__(self, text):
-        self.text = text
-    
+    """the start of it all"""
     def english_to_french(self):
+        """Takes in a word and uses IBM method to transform to fr"""
         result = translator.translate(text=[self], model_id="en-fr").get_result()
         return result['translations'][0]['translation']
-    
     def french_to_english(self):
+        """ Takes in a word """
         result = translator.translate(text=[self], model_id="fr-en").get_result()
         return result['translations'][0]['translation']
